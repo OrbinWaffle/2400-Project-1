60f49d0d74e8c7b708ab28b221286deef9f86a5f
pesimelaki1 <49598484+pesimelaki1@users.noreply.github.com>
2021-09-26 03:23:23 -0700

    improved user readability

--- project1/ArrayBagTest.java
+++ project1/ArrayBagTest.java
@@ -1,22 +1,27 @@
 package project1;
 
-public class ArrayBagTest
+public class LinkedBagTest
 {
    public static void main(String[] args)
    {
-      BagInterface<String> bag1 = new ResizableArrayBag<>();
-      BagInterface<String> bag2 = new ResizableArrayBag<>();
+      BagInterface<String> bag1 = new LinkedBag<>();
+      BagInterface<String> bag2 = new LinkedBag<>();
       String[] items1 = {"a", "b", "c"};
       String[] items2 = {"b", "b", "d", "e"};
       testAdd(bag1, items1);
       testAdd(bag2, items2);
 
+      //Tests Union, Intersection, and Difference methods
+      System.out.println("\nUnion method called:");
       BagInterface<String> newBag = bag1.union(bag2);
       displayBag(newBag);
+      System.out.println("\nIntersection method called:");
       newBag = bag1.intersection(bag2);
       displayBag(newBag);
+      System.out.println("\nDifference method performed on the first bag: ");
       newBag = bag1.difference(bag2);
       displayBag(newBag);
+      System.out.println("\nDifference method performed on the second bag: ");
       newBag = bag2.difference(bag1);
       displayBag(newBag);
 
@@ -32,10 +37,11 @@ public class ArrayBagTest
 		} // end for
       System.out.println();
 	} // end testAdd
+    
    // Tests the method toArray while displaying the bag.
 	private static void displayBag(BagInterface<String> aBag)
 	{
-		System.out.println("The bag contains " + aBag.getCurrentSize() +
+		System.out.println("The new bag contains " + aBag.getCurrentSize() +
 		                   " string(s), as follows:");		
 		Object[] bagArray = aBag.toArray();
 		for (int index = 0; index < bagArray.length; index++)
